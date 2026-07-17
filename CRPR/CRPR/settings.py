f17c7184b186a9ebc10df8d358c148078b6fda6e
JosephNderitu <gikurujoseph53@gmail.com>
2026-07-17 16:29:18 +0300

    Ignore venv and add core project files

--- CRPR/CRPR/settings.py
+++ CRPR/CRPR/settings.py
@@ -152,7 +152,7 @@ WSGI_APPLICATION = 'CRPR.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
